6372cce9aab8c2568006a241b718b2bb9b70c557
rodrigo baptista <rodrigo.baptista@zup.com.br>
2024-09-27 18:08:52 -0300

    Updated to output run_data

--- runtime.py
+++ runtime.py
@@ -1,13 +1,20 @@
 import os
 import requests
 
+def save_output(name: str, value: str):
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as output_file:
+        print(f'{name}={value}', file=output_file)
+
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_KEY = os.getenv("CLIENT_KEY")
 CLIENT_REALM = os.getenv("CLIENT_REALM")
 RUN_ID = os.getenv("RUN_ID")
 FORCE = os.getenv("FORCE_CANCEL")
 
+
 inputs_list = [CLIENT_ID, CLIENT_KEY, CLIENT_REALM]
+
+API_URL = "https://runtime-manager.dev.stackspot.com"
 
 if None in inputs_list:
     print("- Some mandatory input is empty. Please, check the input list.")
@@ -41,25 +48,39 @@ d1 = login_req.json()
 access_token = d1["access_token"]
 
 # Calling 
-
-cancel_headers = {"Authorization": f"Bearer {access_token}", "Content-Type": "application/json"}
-cancel_run_url=f"https://runtime-manager.dev.stackspot.com/v1/run/cancel/{RUN_ID}?force={FORCE}"
+auth_headers = {"Authorization": f"Bearer {access_token}", "Content-Type": "application/json"}
+cancel_run_url=f"{API_URL}/v1/run/cancel/{RUN_ID}?force=true"
 
 print(f"Requesting Run {RUN_ID} to be cancelled")
 
-cancel_request = requests.post(
-        url=cancel_run_url, 
-        headers=cancel_headers
+try:
+    cancel_request = requests.post(
+            url=cancel_run_url, 
+            headers=auth_headers
+        )
+
+    if cancel_request.status_code == 202:
+        print(f"- RUN {RUN_ID} cancelled successfully!")
+
+    elif cancel_request.status_code == 404:
+        print(f"- RUN {RUN_ID} not found.")
+
+    else:
+        print("- Error cancelling run")
+        print("- Status:", cancel_request.status_code)
+        print("- Error:", cancel_request.reason)
+        exit(1)
+finally:
+    fetch_run_data_url =  f"{API_URL}/v1/run/{RUN_ID}"
+
+    run_data_req = requests.get(
+        headers=auth_headers,
+        url=fetch_run_data_url
     )
 
-if cancel_request.status_code == 202:
-    print(f"- RUN {RUN_ID} cancelled successfully!")
-
-elif cancel_request.status_code == 404:
-    print(f"- RUN {RUN_ID} not found.")
-
-else:
-    print("- Error cancelling run")
-    print("- Status:", cancel_request.status_code)
-    print("- Error:", cancel_request.reason)
-    exit(1)
+    if run_data_req.status_code == 200:
+        save_output('run_data', run_data_req.text)
+    else:
+        print("- Error fetching run data, unable to create output")
+        print("- Status:", run_data_req.status_code)
+        print("- Error:", run_data_req.reason)
